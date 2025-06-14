8a5ceaf0cb463d78c25e1717ef2e0c169c88b34d
nate <nschainblatt@gmail.com>
2025-06-14 12:46:09 -0400

    add additional condition to remove redundant calculations

--- ch1/ex1.12/find-element.scm
+++ ch1/ex1.12/find-element.scm
@@ -3,5 +3,6 @@
 
 (define (pt r c) ; note 0 based indexing
   (cond ((and (= r 0) (= c 0)) 1)
+        ((= r c) 1)
         ((or (< r 0) (< c 0)) 0)
         (else (+ (pt (- r 1) (- c 1)) (pt (- r 1) c)))))
