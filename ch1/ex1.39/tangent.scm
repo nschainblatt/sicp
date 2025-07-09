b0b8e849e886ca2aae516241833b2d25e2304d90
nate <nschainblatt@gmail.com>
2025-07-08 21:16:24 -0400

    complete exercise 1.39

---
+++ ch1/ex1.39/tangent.scm
@@ -0,0 +1,25 @@
+;; Utilize 'cont-fract-iter' to approximate tangent in radians.
+
+(define (main)
+  (tan-cf 5.0 10))
+
+(define (tan-cf x k)
+  (define (n-tangent i)
+    (if (= i 1)
+       x
+        ;; NOTE: Reversing the sign here to not modify the my original 'cont-fract-iter' procedure.
+	;; This is because the tangent continued fraction requires (n-tangent - d-tangent).
+	;; So we are doing (d-tangent + -n-tangent).
+       (- (* x x))))
+  (define (d-tangent i)
+    (+ i (- i 1)))
+  (cont-fract-iter n-tangent d-tangent k))
+
+
+(define (cont-fract-iter n d k)
+  (define (iter i result)
+    (if (= i 1)
+      (/ (n i) result)
+                     ;; NOTE: this is where (d-tangent + -n-tangent) occurs.
+      (iter (- i 1) (+ (d (- i 1)) (/ (n i) result)))))
+  (iter k (d k)))
