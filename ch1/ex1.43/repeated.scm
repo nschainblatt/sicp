6986a1747126c63b800eaccc7e8b2ab460f0128e
nate <nschainblatt@gmail.com>
2025-07-12 23:18:52 -0400

    implement compose from the previous exercise

--- ch1/ex1.43/repeated.scm
+++ ch1/ex1.43/repeated.scm
@@ -3,9 +3,13 @@
 
 (define (repeated f n)
   (lambda (x)
-    (define (iter i result)
-      (if (= i 0) result
-	(iter (- i 1) (f result))))
-    (iter n x)))
+    (define (recur i)
+      (if (= i 1)
+	f
+	(compose f (recur (- i 1)))))
+    ((recur n) x)))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
 
 (define (square x) (* x x))
