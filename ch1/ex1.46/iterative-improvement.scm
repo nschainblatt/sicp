81fa9ce5c69599893dcf458c1b71b646f83cbd50
nate <nschainblatt@gmail.com>
2025-07-13 17:15:36 -0400

    update fixed-point to accept a single guess argument similar to the sqrt procedure
    note that this does one less improvement

--- ch1/ex1.46/iterative-improvement.scm
+++ ch1/ex1.46/iterative-improvement.scm
@@ -13,8 +13,8 @@
 (define tolerance 0.00001)
 
 (define (fixed-point f initial-guess)
-  (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
+  (define (close-enough? guess)
+    (< (abs (- guess (f guess))) tolerance))
   ((iterative-improve close-enough? f) initial-guess))
 
 (define (average-damp f)
@@ -27,9 +27,7 @@
   (lambda (initial-guess)
     (define (try guess)
       (let ((new-guess (improve-guess guess)))
-	 ;; NOTE: sqrt and fixed-point accept 1 and 2 arguments for their good-enough? procedures respectively.
-	 ;; This may cause an issue if the arguments were provided in the wrong order.
-	(if (good-enough? new-guess guess)
+	(if (good-enough? new-guess)
 	  new-guess
 	  (try new-guess))))
     (try initial-guess)))
