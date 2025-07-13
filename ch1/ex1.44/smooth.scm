2bf9edc4d791af9eb9e9242362043f687b2286c2
nate <nschainblatt@gmail.com>
2025-07-12 23:31:03 -0400

    complete exercise 1.44

---
+++ ch1/ex1.44/smooth.scm
@@ -0,0 +1,22 @@
+(define (main)
+  ((repeated (smooth square) 2) 5))
+
+(define (repeated f n)
+  (lambda (x)
+    (define (recur i)
+      (if (= i 1)
+	f
+	(compose f (recur (- i 1)))))
+    ((recur n) x)))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (smooth f)
+  (define dx 0.00001)
+  (lambda (x) (average (f (- x dx)) (f x) (f (+ x dx)))))
+
+(define (average x y z) 
+  (/ (+ x y z) 3))
+
+(define (square x) (* x x))
