7d4ae532776f45b737c3dd7da5d9deab98faedab
nate <nschainblatt@gmail.com>
2025-07-19 14:33:01 -0400

    changing pc's

---
+++ ch2/ex2.6/numbers.scm
@@ -0,0 +1,13 @@
+(define (main)
+  (add-1 2) 3)
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(add-1 zero)
+(add-1 (lambda (f) (lambda (x) x)))
+(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+(lambda (f) (lambda (x) (f (((lambda (x) x) x)))))
+(lambda (f) (lambda (x) (f x)))
