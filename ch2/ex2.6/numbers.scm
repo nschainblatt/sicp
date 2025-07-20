b1e6884821786564e68d82896375fd3277e2aa7c
nate <nschainblatt@gmail.com>
2025-07-20 17:57:50 -0400

    update explanation

--- ch2/ex2.6/numbers.scm
+++ ch2/ex2.6/numbers.scm
@@ -2,6 +2,10 @@
 ;; 'add-1' adds a single procedure 'f' to apply to argument x, with subsequent calls
 ;; adding additional composed invocations of 'f'.
 ;; So the number of calls to 'f' is the value of the positive integer we are representing.
+;; To represent the number, we can pass procedure 'inc' for parameter 'f' to display an actual number
+;; and then for the nested lambda function that accepts parameter 'x' we pass in an initial value of 0.
+;; Then based on the number of calls to 'f', we will increment by 1 that many times, displaying the number
+;; inside the procedure representation.
 
 (define (main)
   (print-number one)
@@ -12,11 +16,12 @@
   (display ((x inc) 0))
   (newline))
 
-;; Notice how this definition related to 'add-1'.
-;; In 'add-1' we only care about adding an additional call to 'f'.
-;; In 'add' we are appending to the the number of calls to f that exist
-;; in the argument 'a'.
-;; This builds: f(f(f(...x)))
+;; Notice how this definition is similar to the other number definitions (zero, one two)
+;; We are returning a new number, and remember that we are representing numbers as procedures.
+;; The new number will be the two numbers in the arguments added together, we have already determined
+;; that the value of a number in this procedure format is the number of calls to procedure 'f'.
+;; So, to get our final number we have to combine each of the arguments calls to 'f'.
+;; Ending up with f(f(f(...x))) if 'a' and 'b' were 'one' and 'two' respectively.
 (define (add a b)
   (lambda (f) (lambda (x) ((a f) ((b f) x)))))
 
@@ -26,6 +31,7 @@
 
 (define two (lambda (f) (lambda (x) (f (f x)))))
 
+;; Notice how we simply add an additional call to 'f' to add one to 'n'.
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
