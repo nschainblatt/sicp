34556ef82172a6f3c3fec3c220826108d0f60907
nate <nschainblatt@gmail.com>
2025-08-02 14:09:46 -0400

    complete exercise 2.29 d

--- ch2/ex2.29/binary-mobile.scm
+++ ch2/ex2.29/binary-mobile.scm
@@ -1,3 +1,13 @@
+;; d.
+;; Changing the representation within the constructors to utilize a cons instead of a list procedure makes the underlying
+;; structure hold the mobile components in a improper list/pair. Before with the list procedure, we guaranteed the storage of all
+;; components in a mobile to be in a proper list.
+;; To handle this change, the selectors must be updated to return the correct components that are stored in a slightly different location.
+;; Components are now pairs of pairs, instead of lists of lists.
+;; This only requires having to remove the extra call to car in the 'right' positioned components in the pair.
+;; My helper procedure 'mobile?' will have to change from a 'list?' to 'pair?' primitive procedure to check if the underlying structure is
+;; a mobile or not.
+
 (define (main)
   (let ((mobile-1 (make-mobile (make-branch 2 1) (make-branch 2 1))))
     (let ((balanced-mobile-1 (make-mobile (make-branch 2 mobile-1)
@@ -10,6 +20,7 @@
                                                                         (make-branch 2 mobile-1)))
                                             (make-branch 2 (make-mobile (make-branch 2 mobile-1)
                                                                         (make-branch 1 mobile-1))))))
+
       (display (balanced? balanced-mobile-1))
       (newline)
       (display (balanced? balanced-mobile-2))
@@ -19,24 +30,23 @@
 
 ;; Note that it is assumed that every mobile has two branches that consist of either a weight or another mobile in it's structure.
 (define (make-mobile left right)
-  (list left right))
+  (cons left right))
 
 (define (make-branch length structure)
-  (list length structure))
-
+  (cons length structure))
 
 ;; a.
 (define (left-branch mobile)
   (car mobile))
 
 (define (right-branch mobile)
-  (car (cdr mobile)))
+  (cdr mobile))
 
 (define (branch-length branch)
   (car branch))
 
 (define (branch-structure branch)
-  (car (cdr branch)))
+  (cdr branch))
 
 ;; b.
 ;; Returns the total weight of every branch in a mobile.
@@ -54,7 +64,7 @@
   (recur mobile))
 
 (define (mobile? m)
-  (list? m))
+  (pair? m))
 
 
 ;; c.
