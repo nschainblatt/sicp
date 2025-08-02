8e76166ef1e79018a4f70969006fec8015f6e493
nate <nschainblatt@gmail.com>
2025-08-02 14:43:38 -0400

    refactor exercise 2.29 d

--- ch2/ex2.29/binary-mobile.scm
+++ ch2/ex2.29/binary-mobile.scm
@@ -5,8 +5,6 @@
 ;; To handle this change, the selectors must be updated to return the correct components that are stored in a slightly different location.
 ;; Components are now pairs of pairs, instead of lists of lists.
 ;; This only requires having to remove the extra call to car in the 'right' positioned components in the pair.
-;; My helper procedure 'mobile?' will have to change from a 'list?' to 'pair?' primitive procedure to check if the underlying structure is
-;; a mobile or not.
 
 (define (main)
   (let ((mobile-1 (make-mobile (make-branch 2 1) (make-branch 2 1))))
@@ -51,35 +49,23 @@
 ;; b.
 ;; Returns the total weight of every branch in a mobile.
 (define (total-weight mobile)
-
-  ;; Recursively traverse a mobile, accumulating all of it's weights (leaves of the tree).
-  (define (recur sub-mobile)
-    (let ((left (left-branch sub-mobile))
-          (right (right-branch sub-mobile)))
-
-      ;; The left or the right branch may have as it's structure another mobile
-      (+ (if (mobile? (branch-structure left)) (total-weight (branch-structure left)) (branch-structure left))
-         (if (mobile? (branch-structure right)) (total-weight (branch-structure right)) (branch-structure right)))))
-
-  (recur mobile))
-
-(define (mobile? m)
-  (pair? m))
-
+  (if (number? mobile)
+    mobile
+    (let ((left (left-branch mobile))
+          (right (right-branch mobile)))
+      (+ (total-weight (branch-structure left))
+         (total-weight (branch-structure right))))))
 
 ;; c.
 ;; Returns true if a mobiles branches are balanced.
 (define (balanced? mobile)
+  (if (number? mobile)
+    #t
+    (let ((left (left-branch mobile))
+          (right (right-branch mobile)))
+      (and (= (torque left) (torque right))
+           (balanced? (branch-structure left))
+           (balanced? (branch-structure right))))))
 
-  (let ((left (left-branch mobile))
-        (right (right-branch mobile)))
-
-    (let ((total-left-weight (if (mobile? (branch-structure left)) (total-weight (branch-structure left)) (branch-structure left)))
-          (total-right-weight (if (mobile? (branch-structure right)) (total-weight (branch-structure right)) (branch-structure right)))
-          (curr-left-length (branch-length left))
-          (curr-right-length (branch-length right)))
-
-      (if (= (* total-left-weight curr-left-length) (* total-right-weight curr-right-length))
-        (and (if (mobile? (branch-structure (left-branch mobile))) (balanced? (branch-structure (left-branch mobile))) #t) 
-             (if (mobile? (branch-structure (right-branch mobile))) (balanced? (branch-structure (right-branch mobile))) #t))
-        #f))))
+(define (torque branch)
+  (* (branch-length branch) (total-weight (branch-structure branch))))
