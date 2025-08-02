9d64a95b208516942cad9de23dab3b1a5ad19631
nate <nschainblatt@gmail.com>
2025-08-02 15:07:40 -0400

    complete exercise 2.30

---
+++ ch2/ex2.30/square-tree.scm
@@ -0,0 +1,22 @@
+(define (main)
+  (square-tree (list 1
+                     (list 2 (list 3 4) 5)
+                     (list 6 7))))
+
+(define (square-tree tree)
+  (tree-map square tree))
+
+;; Applies the given procedure to every leaf of the tree
+(define (tree-map fn tree)
+  (cond ((null? tree) '())
+        ((pair? tree) (cons (tree-map fn (car tree)) (tree-map fn (cdr tree))))
+        (else (fn tree))))
+
+(define (square x) (* x x))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (cond ((null? sub-tree) '())
+               ((pair? sub-tree) (square-tree sub-tree))
+               (else (square sub-tree))))
+       tree))
