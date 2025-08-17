e3630d2a18f84d3457b6b623473cd0ae549f96ec
nate <nschainblatt@gmail.com>
2025-08-17 15:58:27 -0400

    fix edge order

--- ch2/ex2.50/transform.scm
+++ ch2/ex2.50/transform.scm
@@ -2,7 +2,7 @@
   (transform-painter painter (make-vect 1 0) (make-vect 0 0) (make-vect 1 1)))
 
 (define (180-counter painter)
-  (transform-painter painter (make-vect 1 1) (make-vect 1 0) (make-vect 0 1)))
+  (transform-painter painter (make-vect 1 1) (make-vect 0 1) (make-vect 1 0)))
 
 (define (270-counter painter)
   (transform-painter painter (make-vect 0 1) (make-vect 0 0) (make-vect 1 1)))
