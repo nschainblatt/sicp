35fb411e398b01578ef1273b13a388c273b20594
nate <nschainblatt@gmail.com>
2025-08-17 14:49:51 -0400

    complete exercise 2.50

---
+++ ch2/ex2.50/transform.scm
@@ -0,0 +1,20 @@
+(define (flip-horiz painter)
+  (transform-painter painter (make-vect 1 0) (make-vect 0 0) (make-vect 1 1)))
+
+(define (180-counter painter)
+  (transform-painter painter (make-vect 1 1) (make-vect 1 0) (make-vect 0 1)))
+
+(define (270-counter painter)
+  (transform-painter painter (make-vect 0 1) (make-vect 0 0) (make-vect 1 1)))
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame
+                   new-origin
+                   (sub-vect (m corner1) new-origin)
+                   (sub-vect (m corner2) new-origin)))))))
+
+(define (make-vect x y)
+  (cons x y))
