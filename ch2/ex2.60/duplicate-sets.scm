0369c1c0e82c704b948d508ec2cdf6a63e41ee55
nate <nschainblatt@gmail.com>
2025-08-25 20:58:41 -0400

    refactor to use accumulate

--- ch2/ex2.60/duplicate-sets.scm
+++ ch2/ex2.60/duplicate-sets.scm
@@ -18,21 +18,26 @@
 (define (adjoin-set x set)
   (cons x set))
 
+(define (custom-adjoin-set bi-predicate)
+  (lambda (x set)
+    (if (bi-predicate x set)
+      set
+      (cons x set))))
+
 ;; Even if the set now allows duplicates, the union operation will still need to return a set that has no duplicates because of how
 ;; union is defined. This was updated to remove the use of adjoin-set since the new version of adjoin-set defined above would allow
 ;; duplicates. The efficiency of this procedure is quadratic.
 (define (union-set set1 set2)
-  (define (iter sub-set new-set)
-    (cond ((null? sub-set) new-set)
-          ((element-of-set? (car sub-set) new-set) (iter (cdr sub-set) new-set))
-          (else (iter (cdr sub-set) (cons (car sub-set) new-set)))))
-  (iter set1 (iter set2 '())))
+  (accumulate (custom-adjoin-set element-of-set?) (accumulate (custom-adjoin-set element-of-set?) '() set2) set1))
 
 ;; As with unions, this also returns a set that has no duplicates.
 ;; The efficiency of this procedure is quadratic.
 (define (intersection-set set1 set2)
-  (define (iter sub-set2 new-set)
-    (cond ((null? sub-set2) new-set)
-          ((and (element-of-set? (car sub-set2) set1) (not (element-of-set? (car sub-set2) new-set))) (iter (cdr sub-set2) (cons (car sub-set2) new-set)))
-          (else (iter (cdr sub-set2) new-set))))
-  (iter set2 '()))
+  (define (bi-predicate x set)
+    (or (not (element-of-set? x set1)) (element-of-set? x set)))
+  (accumulate (custom-adjoin-set bi-predicate) '() set2))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence) (accumulate op initial (cdr sequence)))))
