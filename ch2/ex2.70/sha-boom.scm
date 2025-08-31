f7551b42181710a64e248a9ecb30a170e6398149
nate <nschainblatt@gmail.com>
2025-08-31 17:37:52 -0400

    complete exercise 2.70

---
+++ ch2/ex2.70/sha-boom.scm
@@ -0,0 +1,91 @@
+;; 84 bits are required for the variable length encoding.
+;; The smallest number of bits required if we used fixed length code would be 108, 3 bits per symbol for a total of 36 symbols in the sample message.
+
+(define (main)
+  (let ((pairs '((A 2) (NA 16) (BOOM 1) (SHA 3) (GET 2) (YIP 9) (JOB 2) (WAH 1)))
+        (sample-message '(GET A JOB SHA NA NA NA NA NA NA NA NA GET A JOB SHA NA NA NA NA NA NA NA NA WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP SHA BOOM)))
+    (println (length (encode sample-message (generate-huffman-tree pairs))))))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge pairs)
+  (if (= (length pairs) 1)
+    (car pairs)
+    (let ((first (car pairs))
+          (second (cadr pairs)))
+      (cond ((leaf? first) (successive-merge (adjoin-set (make-code-tree first second) (cddr pairs))))
+            (else (successive-merge (adjoin-set (make-code-tree second first) (cddr pairs))))))))
+
+(define (encode message tree)
+  (if (null? message)
+    '()
+    (append (encode-symbol (car message) tree)
+            (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (cond ((and (leaf? tree) (eq? (symbol-leaf tree) symbol)) '())
+        ((and (not (leaf? tree)) (symbol-in-set? symbol (symbolz (left-branch tree)))) (cons 0 (encode-symbol symbol (left-branch tree))))
+        ((and (not (leaf? tree)) (symbol-in-set? symbol (symbolz (right-branch tree)))) (cons 1 (encode-symbol symbol (right-branch tree))))
+        (else (error "symbol not found in code tree"))))
+
+(define (symbol-in-set? symbol set)
+  (cond ((null? set) #f)
+        ((eq? (car set) symbol) #t)
+        (else (symbol-in-set? symbol (cdr set)))))
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (symbolz tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbolz left) (symbolz right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree)
+  (car tree))
+(define (right-branch tree) (cadr tree))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+      '()
+      (let ((next-branch
+              (choose-branch (car bits) current-branch)))
+        (if (leaf? next-branch)
+          (cons (symbol-leaf next-branch)
+                (decode-1 (cdr bits) tree))
+          (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+    '()
+    (let ((pair (car pairs)))
+      (adjoin-set (make-leaf (car pair) ; symbol
+                             (cadr pair)) ; frequency
+                  (make-leaf-set (cdr pairs))))))
