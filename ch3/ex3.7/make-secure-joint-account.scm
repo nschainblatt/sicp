6677c8eec40fd654731ae67bee95f34619427ac7
nate <nschainblatt@gmail.com>
2025-10-05 20:06:14 -0400

    complete exercise 3.7

---
+++ ch3/ex3.7/make-secure-joint-account.scm
@@ -0,0 +1,38 @@
+(define (main)
+  (define peter-acc (make-account 100 'open-sesame))
+  (define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+
+  (println ((peter-acc 'open-sesame 'withdraw) 40)) ;; 60
+  (println ((paul-acc 'rosebud 'withdraw) 50))      ;; 10
+
+  (println ((peter-acc 'rosebud 'withdraw) 40))     ;; Incorrect password
+  (println ((paul-acc 'open-sesame 'withdraw) 50)))  ;; "
+
+(define (make-joint acc acc-password joint-acc-password)
+  (define (withdraw amount)
+    ((acc acc-password 'withdraw) amount))
+  (define (deposit amount)
+    (acc acc-password 'deposit) amount)
+  (define (dispatch attempted-password m)
+    (cond ((not (eq? attempted-password joint-acc-password)) (error "Incorrect password --MAKE-JOINT-ACCOUNT"))
+	  ((eq? m 'withdraw) withdraw)
+	  ((eq? m 'deposit) deposit)
+	  (else (error "Unknown request: MAKE-JOINT-ACCOUNT" m))))
+  dispatch)
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+	     balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch attempted-password m)
+    (cond ((not (eq? attempted-password password)) (error "Incorrect password --MAKE-ACCOUNT"))
+	  ((eq? m 'withdraw) withdraw)
+	  ((eq? m 'deposit) deposit)
+	  (else (error "Unknown request: MAKE-ACCOUNT" m))))
+  dispatch)
+
