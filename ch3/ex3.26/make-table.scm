afb95173e930ca17c9d1371ebea0616c63f45b24
nate <nschainblatt@gmail.com>
2025-10-18 14:51:53 -0400

    progress making prototype tables with trees

--- ch3/ex3.26/make-table.scm
+++ ch3/ex3.26/make-table.scm
@@ -2,6 +2,8 @@
 ;; 1. Update the table and sub-table representation to be a binary tree instead of just a list of records.
 ;;    - We will need a representation for a tree as well as selectors and a constructor.
 ;;    - We will need a procedure to create a tree from a list, to then use a procedure to turn a list into a balanced tree.
+;;        - This is so we can keep a balanced tree for maximum performance, we can rebalance the tree using an internal counter,
+;;          that tracks the number of inserts! made.
 ;; 2. Update insert! to insert the keys in order from smallest to biggest (numerically or string comparison).
 ;;    - Whenever we create a new table, we need to create a binary tree instead of just a list.
 ;;    - Update the iter inside of insert! to traverse the binary tree and sub-table trees.
@@ -24,14 +26,10 @@
   (define table (make-table equal?))
 
   ((table 'insert) (list 1 1) 'x1)
-  (table 'print)
   ((table 'insert) (list 1 2) 'x2)
-  (table 'print)
   ((table 'insert) (list 1 3) 'x3)
-  (table 'print)
 
   ((table 'insert) (list 2 1) 'y1)
-  (table 'print)
   ((table 'insert) (list 2 2) 'y2)
   ((table 'insert) (list 2 3) 'y3)
 
@@ -43,13 +41,13 @@
   ((table 'insert) (list 2 2) 'y2.2)
   ((table 'insert) (list 3 3) 'z3.3)
 
-  (table 'print)
-
   ((table 'insert) (list 10 9 8 7 6 5 4 3 2 1) 'TEST)
   ((table 'insert) (list 10 9 8 7 6 5 4 3 2 1) 'TESTAGAIN)
 
-  (println ((table 'lookup) (list 10 9 8 7 6 5 4 3 2 1)))
-  (println ((table 'lookup) (list 10 9 8 7 6 5 4 3 2)))
+  (table 'print)
+
+  ; (println ((table 'lookup) (list 10 9 8 7 6 5 4 3 2 1)))
+  ; (println ((table 'lookup) (list 10 9 8 7 6 5 4 3 2)))
 
   'done)
 
@@ -68,6 +66,15 @@
   (define (set-record-value! record value) (set-cdr! record value))
   (define (table? potential-table) (pair? (cdr potential-table)))
 
+  (define (same-entry? key entry)
+    (equal? key (car entry)))
+
+  (define (greater-than-entry? value entry)
+    (> value (car entry)))
+
+  (define (lesser-than-entry? value entry)
+    (< value (car entry)))
+
   (let ((table (list '*table*)))
 
     ;; Note that an unexpected error could happen if keys were given in the wrong order, such as if you gave 3 keys, and the middle
@@ -78,14 +85,18 @@
       (define (insert-remaining-keys sub-keys prev-table)
 	(cond ((null? sub-keys) 'done) ;; This wouldn't happen unless sub-keys was originally null passed in
 	      ((null? (cdr sub-keys))
-	       ;; car is the last key, so set to the new record instead of a table.
-	       (set-cdr! prev-table (cons (make-record (car sub-keys) value) (cdr prev-table)))
+	       ;; If no tree create a new tree, otherwise insert record into existing tree.
+	       (if (null? (cdr prev-table))
+		 (set-cdr! prev-table (make-tree (make-record (car sub-keys) value) '() '() same-entry? greater-than-entry? lesser-than-entry?))
+		 (((cdr prev-table) 'insert!) (make-record (car sub-keys) value)))
 	       'done)
 	      (else
 		;; Insert a new table, and proceed to the next key
-		(let ((new-table (cons (car sub-keys) '())))
-		  (set-cdr! prev-table (cons new-table (cdr prev-table)))
-		  (insert-remaining-keys (cdr sub-keys) new-table)))))
+		(let* ((new-pair (cons (car sub-keys) '()))
+		      (new-tree (make-tree new-pair '() '() same-entry? greater-than-entry? lesser-than-entry?)))
+		  (if (null? (cdr prev-table))
+		    (begin (set-cdr! prev-table new-tree) (insert-remaining-keys (cdr sub-keys) (new-tree 'entry)))
+		    (begin (((cdr prev-table) 'insert!) new-pair) (insert-remaining-keys (cdr sub-keys) new-pair)))))))
 
       (define (iter sub-keys sub-table)
 	(cond ((null? sub-keys)
@@ -112,22 +123,23 @@
       (iter keys table))
 
     ;; Returns the record under the key, or false if none found.
-    (define (myassoc key sub-table)
-      (cond ((null? sub-table) #f)
-	    ((same-key? key (caar sub-table)) (car sub-table))
-	    (else (myassoc key (cdr sub-table)))))
+    ;; NOTE: requires each sub-tree to have a procedure to perform an appropriate same-key? comparison by extracting the
+    ;; key from the record within the sub-trees entry and comparing it to the target key we pass in here.
+    (define (myassoc key sub-tree)
+      (cond ((null? sub-tree) #f)
+	    (else ((sub-tree 'lookup) key))))
 
     (define (print)
-      (define (print-iter sub-table)
-	(if (null? sub-table)
-	  '()
-	  (begin (println (car sub-table)) (print-iter (cdr sub-table)))))
-      (print-iter table))
+      (define (iter sub-table)
+	(if (not (null? (cdr sub-table)))
+	  ((cdr sub-table) 'print)
+	  'done))
+      (iter table))
 
     (define (dispatch m)
       (cond ((eq? m 'insert) insert!)
 	    ((eq? m 'lookup) lookup)
-	    ((eq? m 'print) (println table))
+	    ((eq? m 'print) (print))
 	    (else (error "Invalid operation MAKE-TABLE"))))
     dispatch))
 
@@ -140,25 +152,30 @@
     (define (entry) (car tree))
     (define (left-branch) (cadr tree))
     (define (right-branch) (cddr tree))
+    (define (set-entry! value)
+      (set-car! tree value))
     (define (set-left-branch! value)
       (set-car! (cdr tree) value))
     (define (set-right-branch! value)
       (set-cdr! (cdr tree) value))
- 
+
+    (define (insert! target-entry)
+      (cond ((same-entry? (car target-entry) (entry)) (set-entry! target-entry))
+	    ((greater-than-entry? (car target-entry) (entry))
+	     (if (null? (right-branch)) (set-right-branch! (make-tree target-entry '() '() same-entry? greater-than-entry? lesser-than-entry?)) (((right-branch) 'insert!) target-entry)))
+	    ((lesser-than-entry? (car target-entry) (entry))
+	     (if (null? (left-branch)) (set-left-branch! (make-tree target-entry '() '() same-entry? greater-than-entry? lesser-than-entry?)) (((left-branch) 'insert!) target-entry)))
+	    (else ("Illegal state exception"))))
+
     ;; NOTE: only works with binary trees (balanced or unbalanced, preferably balanced for best performance)
     (define (lookup target-entry)
-      (cond ((same-entry? target-entry (entry)) #t)
+      (cond ((same-entry? target-entry (entry)) (entry))
 	    ((and (greater-than-entry? target-entry (entry)) (not (null? (right-branch)))) (((right-branch) 'lookup) target-entry))
 	    ((and (lesser-than-entry? target-entry (entry)) (not (null? (left-branch)))) (((left-branch) 'lookup) target-entry))
 	    (else #f)))
 
-    ;; (trace same-entry?)
-    ;; (trace greater-than-entry?)
-    ;; (trace lesser-than-entry?)
-
-
     ;; Prints in ascending order if the the tree is binary.
-    (define (print tree)
+    (define (print)
       (if (or (null? tree))
 	(println "this shouldn't happen")
 	(let ((left (left-branch))
@@ -167,19 +184,25 @@
 	  (if (null? left)
 	    'done
 	    (left 'print))
-	  (println entry)
+	  (if (tree? (cdr entry))
+	    (begin (println (car entry)) ((cdr entry) 'print))
+	    (println entry))
 	  (if (null? right)
 	    'done
-	    (right 'print)))))
+	    (right 'print))))
+      dispatch)
     (define (dispatch m)
       (cond ((eq? m 'entry) (entry))
 	    ((eq? m 'left-branch) (left-branch))
-	    ((eq? m 'rigth-branch) (right-branch))
+	    ((eq? m 'right-branch) (right-branch))
 	    ((eq? m 'set-left-branch!) set-left-branch!)
 	    ((eq? m 'set-right-branch!) set-right-branch!)
 	    ((eq? m 'lookup) lookup)
-	    ((eq? m 'print) (print tree))
+	    ((eq? m 'insert!) insert!)
+	    ((eq? m 'print) (print))
 	    (else (error "unknown operation TREE"))))
     dispatch))
+
+(define tree? procedure?)
 
 (main)
