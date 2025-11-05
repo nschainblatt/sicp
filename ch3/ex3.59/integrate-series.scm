2e8d18d18d94e55508b4a0b844bd99921fc7d029
nate <nschainblatt@gmail.com>
2025-11-04 19:20:18 -0500

    complete exercise 3.59 part a

---
+++ ch3/ex3.59/integrate-series.scm
@@ -0,0 +1,31 @@
+(define (integrate-series stream-a)
+  (stream-map-with-index (lambda (index value) (* (/ 1 index) value)) stream-a))
+
+(define (stream-map-with-index proc s)
+  (define (recur sub index)
+    (if (null? sub)
+      '()
+      (cons-stream (proc index (stream-car sub)) (recur (stream-cdr sub) (+ index 1)))))
+  (recur s 1))
+
+(define (stream-for-each-until proc s ref)
+  (cond ((null? s) 'done)
+	((< ref 1) 'done)
+	(else (proc (stream-car s)) (stream-for-each-until proc (stream-cdr s) (- ref 1)))))
+
+(define (print x)
+  (display x) (display " "))
+
+(define (println x)
+  (newline) (display x))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high) '()
+    (cons-stream low (stream-enumerate-interval (+ low 1) high))))
+
+(define s1 (stream-enumerate-interval 3 20))
+
+(newline)
+(stream-for-each-until print s1 10)
+(newline)
+(stream-for-each-until print (integrate-series s1) 10)
