313c8e51bb03c45808e1badd42feadbab0f928a5
nate <nschainblatt@gmail.com>
2025-11-08 15:15:24 -0500

    complete exercise 3.67

---
+++ ch3/ex3.67/pairs.scm
@@ -0,0 +1,27 @@
+(define (pairs s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (interleave
+      (stream-map (lambda (x) (list x (stream-car t))) (stream-cdr s))
+      (interleave
+	(stream-map (lambda (x) (list (stream-car s) x)) (stream-cdr t))
+	(pairs (stream-cdr s) (stream-cdr t))))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+    s2
+    (cons-stream (stream-car s1)
+		 (interleave s2 (stream-cdr s1)))))
+
+(define integers
+  (cons-stream 1 (stream-map (lambda (x) (+ x 1)) integers)))
+
+(define (print-n-stream stream n)
+  (newline)
+  (define (iter s i)
+    (if (< i n)
+      (begin (display (stream-car s)) (display " ") (iter (stream-cdr s) (+ i 1)))
+      'done))
+  (iter stream 0))
+
+(print-n-stream (pairs integers integers) 100)
