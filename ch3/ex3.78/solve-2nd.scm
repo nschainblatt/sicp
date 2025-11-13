6220037c2a8a2720de956ab9cebe1a66caa9ac11
nate <nschainblatt@gmail.com>
2025-11-12 19:43:17 -0500

    complete exercise 3.78

---
+++ ch3/ex3.78/solve-2nd.scm
@@ -0,0 +1,35 @@
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream
+      initial-value
+      (let ((integrand (force delayed-integrand)))
+	(add-streams (scale-stream integrand dt) int))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(define (scale-stream s factor)
+  (stream-map (lambda (x) (* x factor)) s))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (solve-2nd a b dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (add-streams (scale-stream dy a) (scale-stream y b)))
+  y)
+
+(define (print-n-stream stream n)
+  (newline)
+  (define (iter s i)
+    (if (< i n)
+      (begin (display (stream-car s)) (display " ") (iter (stream-cdr s) (+ i 1)))
+      'done))
+  (iter stream 0))
+
+(newline)
+(print-n-stream (solve-2nd 1 2 0.001 1 2) 10)
