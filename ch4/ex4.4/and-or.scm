e50bb19ca091386b1acbdf95731e426988eb7b64
nate <nschainblatt@gmail.com>
2025-11-15 16:16:10 -0500

    update to data directed style

--- ch4/ex4.4/and-or.scm
+++ ch4/ex4.4/and-or.scm
@@ -1,48 +1,37 @@
-;; AND
-(define (and? exp)
-  (tagged-list? exp 'and))
-(define and-operands cdr)
-(define and-first-operand car)
-(define and-rest-operands cdr)
-(define and-empty-operands? null?)
-(define (and-last-operand? operands)
-  (and (not (null? operands)) (null? (cdr operands))))
+(define (install-and-or-package)
+  ;; AND
+  (define and-first-exp car)
+  (define and-rest-exps cdr)
+  (define and-empty? null?)
+  (define (and-last-operand? expressions)
+    (and (not (null? expressions)) (null? (cdr expressions))))
 
-;; Evaluate operands left to right, if any are false, return false, if no arguments, return true.
-;; If all operands evaluate to true, return the value of the last evaluated operand.
-(define (eval-and exp env)
-  (eval-and-operands (and-operands exp env)))
-(define (eval-and-operands operands env)
-  (if (and-empty-operands? operands)
-    true ;; No operands, return true.
-    (let ((result (eval (and-first-operand operands) env)))
-	    ;; If all operands are true, return value of last operand expression evaluated.
-      (cond ((and (true? result) (and-last-operand operands)) result)
-	    ;; If result is true, but there are more operands, continue.
-	    ((true? result) (eval-and-operands (and-rest-operands operands) env))
-	    (else false))))) ;; Result is false.
+  ;; Evaluate expressions left to right, if any are false, return false, if no arguments, return true.
+  ;; If all expressions evaluate to true, return the value of the last evaluated operand.
+  (define (eval-and expressions env)
+    (if (and-empty? expressions)
+      true ;; No expressions, return true.
+      (let ((result (eval (and-first-exp expressions) env)))
+	;; If all expressions are true, return value of last operand expression evaluated.
+	(cond ((and (true? result) (and-last-operand expressions)) result)
+	      ;; If result is true, but there are more expressions, continue.
+	      ((true? result) (eval-and (and-rest-exps expressions) env))
+	      (else false))))) ;; Result is false.
 
+  ;; OR
+  (define or-first-exp car)
+  (define or-rest-exps cdr)
+  (define or-empty? null?)
 
+  ;; Evaluate expressions left to right, if any are true, return that value.
+  ;; If all expressions evaluate to false, or if there are no expressions, return false.
+  (define (eval-or expressions env)
+    (if (or-empty? expressions)
+      false
+      (let ((result (eval (or-first-exp expressions) env)))
+	(if (true? result)
+	  result
+	  (eval-or (or-rest-exps expressions) env)))))
 
-;; OR
-(define (or? exp)
-  (tagged-list? exp 'or))
-(define or-operands cdr)
-(define or-first-operand car)
-(define or-rest-operands cdr)
-(define or-empty-operands? null?)
-
-;; Evaluate operands left to right, if any are true, return that value.
-;; If all operands evaluate to false, or if there are no operands, return false.
-(define (eval-or exp env)
-  (eval-or-operands (or-operands exp) env))
-(define (eval-or-operands operands env)
-  (if (or-empty-operands? operands)
-    false
-    (let ((result (eval (or-first-operand operands) env)))
-      (if (true? result)
-	result
-	(eval-or-operands (or-rest-operands operands) env)))))
-
-(define true #t)
-(define false #f)
+  (put 'eval 'and eval-and)
+  (put 'eval 'or eval-or))
