80eff9e3b5b5ea8217410722c835192480a1793c
nate <nschainblatt@gmail.com>
2025-11-19 20:35:16 -0500

    create named let representation

---
+++ ch4/ex4.8/named-let.scm
@@ -0,0 +1,93 @@
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp) (lookup-variable-value exp env))
+	((quoted? exp) (text-of-quotation exp))
+	((assignment? exp) (eval-assignment exp env))
+	((definition? exp) (eval-definition exp env))
+	((if? exp) (eval-if exp env))
+	((lambda? exp) (make-procedure (lambda-parameters exp)
+				       (lambda-body exp)
+				       env))
+	((begin? exp)
+	 (eval-sequence (begin-actions exp) env))
+	((cond? exp) (eval (cond->if exp) env))
+	((let? exp) (eval (let->combination exp) env))
+	((application? exp)
+	 (apply (eval (operator exp) env)
+		(list-of-values (operands exp) env)))
+	(else
+	  (error "Unknown expression type: EVAL" exp))))
+
+;; Let syntax
+;; (let ((⟨var1⟩ ⟨exp1⟩) . . . (⟨varn⟩ ⟨expn⟩))
+;;   ⟨body⟩)
+(define (let? exp)
+  (tagged-list? exp 'let))
+(define (make-let assignments body)
+  (cons 'let (list assignments body)))
+(define (make-named-let name assignments body)
+  (cons 'let (list assignments body name)))
+(define (named-let? exp)
+  (not (null? (cdddr exp))))
+(define (let-name exp)
+  (cadddr exp))
+(define (let-assignments exp)
+  (cadr exp))
+(define (let-body exp)
+  (caddr exp))
+(define (let-first-assignment assignments)
+  (car assignments))
+(define (let-rest-assignments assignments)
+  (cdr assignments))
+(define (let-variable assignment)
+  (car assignment))
+(define (let-value assignment)
+  (cadr assignment))
+(define (empty-assignments? assignments)
+  (null? assignments))
+
+(define (let->combination exp)
+
+  (define (let-assignments->variables-values-pair assignments)
+    (define (iter variables values sub-assignments)
+      (if (empty-assignments? sub-assignments)
+	(cons variables values)
+	(let* ((assignment (let-first-assignment sub-assignments))
+	       (variable (let-variable assignment))
+	       (value (let-value assignment)))
+	  (iter (append variables (list variable)) (append values (list value)) (let-rest-assignments sub-assignments)))))
+    (iter '() '() assignments))
+
+  (let* ((variables-values-pair (let-assignments->variables-values-pair (let-assignments exp)))
+	 (variables (car variables-values-pair))
+	 (list-of-values (cdr variables-values-pair))
+	 (Lambda (make-lambda variables (let-body exp))))
+
+    (cons Lambda list-of-values)))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define (println x)
+  (newline)
+  (display x))
+
+(define example-let-expression
+  (make-let (list '(x 1) '(y 2)) '(+ x y)))
+
+;; #f
+(println (named-let? example-let-expression))
+;; (let ((x 1) (y 2)) (+ x y))
+(println example-let-expression)
+;; ((lambda (x y) + x y) 1 2)
+(println (let->combination example-let-expression))
+(newline)
+
+(define example-named-let-expression
+  (make-named-let 'test-proc (list '(x 1) '(y 2)) '(+ x y)))
+
+;; #t
+(println (named-let? example-named-let-expression))
+(println example-named-let-expression)
+(println (let->combination example-named-let-expression))
+(newline)
