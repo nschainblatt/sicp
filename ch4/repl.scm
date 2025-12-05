aa1576bc7d8b65b2c13bba66685bdec61572eeee
nate <nschainblatt@gmail.com>
2025-12-04 20:04:40 -0500

    revert back to applicative order repl

--- ch4/repl.scm
+++ ch4/repl.scm
@@ -98,78 +98,30 @@
   (put 'eval 'lambda (lambda (exp env) (make-procedure (lambda-parameters exp) (lambda-body exp) env)))
   (put 'eval 'begin (lambda (exp env) (eval-sequence (begin-actions exp) env)))
   (put 'eval 'cond (lambda (exp env) (eval (cond->if exp) env)))
-  (put 'eval 'call (lambda (exp env) (apply (actual-value (operator exp) env)
-					    (operands exp)
-					    env)))
+  (put 'eval 'call (lambda (exp env) (apply (eval (operator exp) env) (list-of-values (operands exp) env))))
   (put 'eval 'let (lambda (exp env) (eval (let->combination exp) env)))
   )
 
-(define (actual-value exp env)
-  (force-it (eval exp env)))
-
-(define (apply procedure arguments env)
+(define (apply procedure arguments)
   (cond ((primitive-procedure? procedure)
-	 (apply-primitive-procedure
-	   procedure
-	   (list-of-arg-values arguments env))) ; changed
+	 (apply-primitive-procedure procedure arguments))
 	((compound-procedure? procedure)
-	 (eval-sequence
-	   (procedure-body procedure)
-	   (extend-environment
-	     (procedure-parameters procedure)
-	     (list-of-delayed-args arguments env) ; changed
-	     (procedure-environment procedure))))
-	(else (error "Unknown procedure type: APPLY"
-		     procedure))))
-
-(define (list-of-arg-values exps env)
-  (if (no-operands? exps)
-    '()
-    (cons (actual-value (first-operand exps)
-			env)
-	  (list-of-arg-values (rest-operands exps)
-			      env))))
-(define (list-of-delayed-args exps env)
-  (if (no-operands? exps)
-    '()
-    (cons (delay-it (first-operand exps)
-		    env)
-	  (list-of-delayed-args (rest-operands exps)
-				env))))
-
-(define (eval-if exp env)
-  (if (true? (actual-value (if-predicate exp) env))
-    (eval (if-consequent exp) env)
-    (eval (if-alternative exp) env)))
-
-(define (force-it obj)
-  (if (thunk? obj)
-    (actual-value (thunk-exp obj) (thunk-env obj))
-    obj))
-
-(define (delay-it exp env)
-  (list 'thunk exp env))
-(define (thunk? obj)
-  (tagged-list? obj 'thunk))
-(define (thunk-exp thunk) (cadr thunk))
-(define (thunk-env thunk) (caddr thunk))
-
-(define (evaluated-thunk? obj)
-  (tagged-list? obj 'evaluated-thunk))
-(define (thunk-value evaluated-thunk)
-  (cadr evaluated-thunk))
-(define (force-it obj)
-  (cond ((thunk? obj)
-	 (let ((result (actual-value (thunk-exp obj)
-				     (thunk-env obj))))
-	   (set-car! obj 'evaluated-thunk)
-	   (set-car! (cdr obj)
-		     result) ; replace exp with its value
-	   (set-cdr! (cdr obj)
-		     '()) ; forget unneeded env
-	   result))
-	((evaluated-thunk? obj) (thunk-value obj))
-	(else obj)))
+	 (let* ((procedure-parts (scan-out-defines (procedure-body procedure)))
+		(parameters (if (null? procedure-parts) (procedure-parameters procedure) (append (car procedure-parts) (procedure-parameters procedure))))
+		(args (if (null? procedure-parts) arguments (append (cadr procedure-parts) arguments)))
+		(body (if (null? procedure-parts) (procedure-body procedure) (caddr procedure-parts))))
+	   (if (null? body)
+	     (error "empty procedure body --APPLY")
+	     (begin
+	       (eval-sequence
+		 body
+		 (extend-environment
+		   parameters
+		   args
+		   (procedure-environment procedure)))))))
+	(else
+	  (error
+	    "Unknown procedure type: APPLY" procedure))))
 
 (define unassigned '*unassigned*)
 
@@ -199,6 +151,11 @@
     '()
     (cons (eval (first-operand exps) env)
 	  (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+    (eval (if-consequent exp) env)
+    (eval (if-alternative exp) env)))
 
 (define (eval-sequence exps env)
   (cond ((last-exp? exps)
@@ -448,7 +405,6 @@
 	(list '> >)
 	(list '< <)
 	(list 'unassigned unassigned)
-	(list 'list list)
 	;; ⟨more primitives⟩
 	))
 (define (primitive-procedure-names)
@@ -461,14 +417,13 @@
   (apply-in-underlying-scheme
     (primitive-implementation proc) args))
 
-(define input-prompt ";;; L-Eval input:")
-(define output-prompt ";;; L-Eval value:")
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+
 (define (driver-loop)
   (prompt-for-input input-prompt)
   (let ((input (read)))
-    (let ((output
-	    (actual-value
-	      input the-global-environment)))
+    (let ((output (eval input the-global-environment)))
       (announce-output output-prompt)
       (user-print output)))
   (driver-loop))
