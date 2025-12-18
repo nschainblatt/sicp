f51b4acc4d7ee67cb0375c333d067b9e0a1a854a
nate <nschainblatt@gmail.com>
2025-12-17 20:34:52 -0500

    complete (sort of) exercise 4.45

--- ch4/ex4.45/grammar.scm
+++ ch4/ex4.45/grammar.scm
@@ -1,11 +1,64 @@
-;; With the grammar given above, the following sentence can be parsed in five different ways:
+(parse '(the professor lectures to the student in the class with the cat))
 
-;;NOTE: “The professor lectures to the student in the class with the cat.”
+;; I actually see 6 ways this sentence could be parsed.
+;; 1. The professor lectures to the student. The student is in the class. The class is with the cat.
+;; 1. The professor lectures to the student. The student is in the class. The student is with the cat.
+;; 2. The professor lectures to the student. The professor is in the class. The professor is with the cat.
+;; 3. The professor lectures to the student. The student is in the class. The professor is with the cat.
+;; 4. The professor lectures to the student. The professor is in the class. The student is with the cat.
+;; 5. The professor lectures to the student. The professor is in the class. The class is with the cat.
+;; 6. The professor lectures to the student. The student is in the class. The student is with the cat.
 
-;; Give the five parses and explain the differences in shades of meaning among them.
+;; I had trouble with this exercise, when I go to see other solutions online, it looks like everyone is using the amb evaluator
+;; we havn't gotten access to yet in the book. This seems to void the point of the exercise, which is to use your current mental modal
+;; of the amb evaluator to do the problem yourself.
 
-;; 1. The professor lectures the student. The professor is with the cat.
-;;    The student is in the class.
+;; I've had numerous pathways, however, with the actual behavior of the amb still left to discover in the book, it is hard to
+;; manually generate the outcomes. I can of course just build the output based off of the permutations of the prepositions in the
+;; input, however this is not the point of the exercise.
+
+;; 1. Note: this description might be wrong: The first parse allows the last usage of 'amb' at the end of each word to greedily parse the next word repeatedly.
+;;    What this means is after the preposition: "to the student", the prepositions following this one go under it recursively
+;;    within it's noun phrase.
+;;    This is because when the parser stops at the end of "student", the null? requirement fails in the sentence parser,
+;;    causing the evaluator to backtrack to the latest usage of 'amb' to try the next option. This is within the noun-parser of the
+;;    latest preposition phrase: "to the student", the noun being "student", the next choice is a non-simple noun phrase
+;;    with the next preposition: "in the class" stored within. This happens again for "with the cat", then the parsing is complete.
+;;    I went ahead and manually created these using what I believe to be all possible outcomes.
+(sentence
+  (simple-noun-phrase (article the) (noun professor))
+  (verb-phrase
+    (verb lectures)
+    (prep-phrase
+      (prep to)
+      (noun-phrase
+	(simple-noun-phrase (article the) (noun student))
+	(prep-phrase
+	  (prep in)
+	  (noun-phrase
+	    (simple-noun-phrase (article the) (noun class))
+	    (prep-phrase
+	      (prep with)
+	      (simple-noun-phrase (article the) (noun cat)))))))))
+
+;; 2.
+(sentence
+  (simple-noun-phrase (article the) (noun professor))
+  (verb-phrase
+    (verb lectures)
+    (prep-phrase
+      (prep to)
+      (noun-phrase
+	(noun-phrase
+	  (simple-noun-phrase (article the) (noun student))
+	  (prep-phrase
+	    (prep in)
+	    (simple-noun-phrase (article the) (noun class))))
+	(prep-phrase
+	  (prep with)
+	  (simple-noun-phrase (article the) (noun cat)))))))
+
+;; 3.
 (sentence
   (simple-noun-phrase (article the) (noun professor))
   (verb-phrase
@@ -22,31 +75,7 @@
       (prep with)
       (simple-noun-phrase (article the) (noun cat)))))
 
-;; 2. The professor lectures the student.
-;;    The student is in the class with the cat.
-(sentence
-  (simple-noun-phrase (article the) (noun professor))
-  (verb-phrase
-    (verb-phrase
-      (verb lectures)
-      (prep-phrase
-	(prep to)
-	(noun-phrase
-	  (simple-noun-phrase (article the) (noun student))
-	  (prep-phrase
-	    (prep in)
-	    (noun-phrase
-	      (simple-noun-phrase (article the) (noun class))
-	      (prep-phrase
-		(prep with)
-		(simple-noun-phrase (article the) (noun cat))))))))))
-
-;; 3. The professor lectures the student. The professor is in the class.
-;;    The student is with the cat
-
-;; --- START
-
-;; 4. The professor lectures the student. The professor is in the class. The professor is with the cat.
+;; 4.
 (sentence
   (simple-noun-phrase (article the) (noun professor))
   (verb-phrase
@@ -55,7 +84,8 @@
 	(verb lectures)
 	(prep-phrase
 	  (prep to)
-	  (simple-noun-phrase (article the) (noun student))))
+	  (noun-phrase
+	    (simple-noun-phrase (article the) (noun student)))))
       (prep-phrase
 	(prep in)
 	(simple-noun-phrase (article the) (noun class))))
@@ -63,54 +93,7 @@
       (prep with)
       (simple-noun-phrase (article the) (noun cat)))))
 
-
-;; 5. With the cat is attached to the professor noun.
-(sentence
-  (noun-phrase
-    (simple-noun-phrase (article the) (noun professor))
-    (prep-phrase
-      (prep with)
-      (simple-noun-phrase (article the) (noun cat))))
-  (verb-phrase
-    (verb-phrase
-      (verb-phrase
-	(verb lectures)
-	(prep-phrase
-	  (prep to)
-	  (simple-noun-phrase (article the) (noun student))))
-      (prep-phrase
-	(prep in)
-	(simple-noun-phrase (article the) (noun class))))))
-
-;; 6.
-(sentence
-  (noun-phrase
-    (simple-noun-phrase (article the) (noun professor))
-    (prep-phrase
-      (prep with)
-      (simple-noun-phrase (article the) (noun cat))))
-  (verb-phrase
-    (verb-phrase
-      (verb-phrase
-	(verb lectures)
-	(prep-phrase
-	  (prep to)
-	  (simple-noun-phrase (article the) (noun student))))
-      (prep-phrase
-	(prep in)
-	(simple-noun-phrase (article the) (noun class))))))
-
-
-;; Starting over
-
-;;NOTE: “The professor lectures to the student in the class with the cat.”
-
-;; 1. Reads normally up to the very lectures. The null requirement fails because there is more input to parse, the most
-;; recent amb is tried again, which is in the verb parser. It attached the next word which is a prep-phrase to the verb.
-;; This happens another time. Then the input is empty.
-;;
-;; The professor lectures to the student. The professor is in the class while lecturing. The professor is with the cat while
-;; lecturing.
+;; 5.
 (sentence
   (simple-noun-phrase (article the) (noun professor))
   (verb-phrase
@@ -119,38 +102,11 @@
 	(verb lectures)
 	(prep-phrase
 	  (prep to)
-	  (simple-noun-phrase (article the) (noun student))))
-      (prep-phrase
-	(prep in)
-	(simple-noun-phrase (article the) (noun class))))
+	  (noun-phrase
+	    (simple-noun-phrase (article the) (noun student))
+	    (prep-phrase
+	      (prep with)
+	      (simple-noun-phrase (article the) (noun cat)))))))
     (prep-phrase
-      (prep with)
-      (simple-noun-phrase (article the) (noun cat)))))
-
-;; 2. To get the second version, we input try-again into the amb evaluator. This will backtrack to the most recent amb and
-;; take a new path (it will also place the latest word back in the input to read, undoing the previous set! operation).
-;; I am assuming that when we 'try-again' that we actually go back to the second to latest amb, which is inside the verb parser, where
-;; the actual most recent amb is inside the noun parser apart of the last prep parsing. I assume this because if we were to feed
-;; 'with the cat' back into the input, and return to the noun amb, we would simply repeat the same pred phrase over and over.
-
-;; NOTE: UPDATE: Remember that we are using APPLICATIVE ORDER EVALUATION. So what may appear to be the latest usage of amb may not be.
-;; The latest usage of amb may be a call higher up the stack, since the arguments are evaluated before amb is applied, the inner usages
-;; of amb aren't necessarily the latest ones, it will likely be the outer ones.
-
-
-;;NOTE: “The professor lectures to the student in the class with the cat.”
-(sentence
-  (simple-noun-phrase (article the) (noun professor))
-  (verb-phrase
-    (verb-phrase
-      (verb-phrase
-	(verb lectures)
-	(prep-phrase
-	  (prep to)
-	  (simple-noun-phrase (article the) (noun student))))
-      (prep-phrase
-	(prep in)
-	(simple-noun-phrase (article the) (noun class))))
-    (prep-phrase
-      (prep with)
-      (simple-noun-phrase (article the) (noun cat)))))
+      (prep in)
+      (simple-noun-phrase (article the) (noun class)))))
