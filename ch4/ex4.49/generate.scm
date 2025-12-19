20458a77a31d5873e770374941f694dcaa4e6f27
Nate Schainblatt <nschainblatt@gmail.com>
2025-12-19 18:49:51 -0500

    complete the first half of exercise 4.49

--- ch4/ex4.49/generate.scm
+++ ch4/ex4.49/generate.scm
@@ -1,11 +1,38 @@
-;; Reverse the parser into a generator
+;; Returns one of the words in the word list wrapped in it's type.
+;; We have to reattach the type for subsequent choices since it's always the first element of a word-list,
+;; and we want to know the type.
+(define (generate-word word-list)
+  (let ((type ((car word-list)))
+	(word ((cadr word-list)))
+	(rest ((caddr word-list))))
+    (amb (list type word) (generate-word (cons type (cddr word-list))))))
 
 
-;; It must follow the same grammatical structure
+;; These parsers were renamed to generators, with no functional different. Just a name change.
+(define (generate-sentence)
+  (list 'sentence (generate-noun-phrase) (generate-verb-phrase)))
 
+(define (generate-simple-noun-phrase)
+  (list 'simple-noun-phrase
+	(generate-word articles)
+	(generate-word nouns)))
 
-;; It will follow this pattern:
+(define (generate-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+	 (maybe-extend
+	   (list 'noun-phrase
+		 noun-phrase
+		 (generate-prepositional-phrase)))))
+  (maybe-extend (generate-simple-noun-phrase)))
 
-;; Build a sentence containing a noun and a verb in that order, ignoring optional words initially.
-;; With subsequent 'try-again' input, different amb branches will be taken that will include the optional words (propositions
-;; for both nouns and verbs).
+(define (generate-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+	 (maybe-extend
+	   (list 'verb-phrase
+		 verb-phrase
+		 (generate-prepositional-phrase)))))
+  (maybe-extend (generate-word verbs)))
+
+;; Example sentences and how they work:
