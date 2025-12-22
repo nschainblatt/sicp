c938e5815c9d56ddc5d1e4c621257ac843c2e482
nate <nschainblatt@gmail.com>
2025-12-21 20:38:27 -0500

    analyze amb

--- ch4/amb-eval/amb-eval.scm
+++ ch4/amb-eval/amb-eval.scm
@@ -152,6 +152,18 @@
           (error "Unknown procedure type: EXECUTE-APPLICATION"
                  proc))))
 
+(define (analyze-amb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+          (fail)
+          ((car choices)
+           env
+           succeed
+           (lambda () (try-next (cdr choices))))))
+      (try-next cprocs))))
+
 
 ;; ---
 
