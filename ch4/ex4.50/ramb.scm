17660a0c2cc78ee8b71e00ccd068ff1838e4af60
nate <nschainblatt@gmail.com>
2025-12-21 22:11:48 -0500

    complete part 2 of exercise 4.50

--- ch4/ex4.50/ramb.scm
+++ ch4/ex4.50/ramb.scm
@@ -2,6 +2,8 @@
 (load "amb-eval.scm")
 
 ;; Task:
+
+;; a.
 ;; Update analyze-amb to choose a choice at random, instead of sequentially moving through options.
 ;; Make sure not to choose the same path twice.
 
@@ -9,6 +11,9 @@
 ;; However we need a way to remove this option from further use.
 ;; We can do this by creating an iterator that builds up a new list as we are going to the random index.
 ;; Then it returns a new list without the current option.
+
+;; b.
+;; Use the changes to help Alyssa in ex4.49 (generate creative non recursive sentences).
 
 (define (analyze-amb exp)
   (let ((cprocs (map analyze (amb-choices exp))))
@@ -41,11 +46,57 @@
 (define (inc x)
   (+ x 1))
 
-(define (println . args)
-  (newline)
-  (for-each (lambda (x) (display x) (display " ")) args))
-
 ;; ---
+;; ex4.49 to copy into the driver loop:
+
+;; Returns one of the words in the word list wrapped in it's type.
+;; We have to reattach the type for subsequent choices since it's always the first element of a word-list,
+;; and we want to know the type.
+(define (generate-word word-list)
+  (if (null? (cdr word-list))
+    (amb)
+    (let ((type (car word-list))
+	  (word (cadr word-list))
+	  (rest (cddr word-list)))
+      (amb (list type word) (generate-word (cons type rest))))))
+
+
+;; These parsers were renamed to generators, with no functional different. Just a name change.
+(define (generate-sentence)
+  (list 'sentence (generate-noun-phrase) (generate-verb-phrase)))
+
+(define (generate-simple-noun-phrase)
+  (list 'simple-noun-phrase
+	(generate-word articles)
+	(generate-word nouns)))
+
+(define (generate-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+	 (maybe-extend
+	   (list 'noun-phrase
+		 noun-phrase
+		 (generate-prepositional-phrase)))))
+  (maybe-extend (generate-simple-noun-phrase)))
+
+(define (generate-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+	 (maybe-extend
+	   (list 'verb-phrase
+		 verb-phrase
+		 (generate-prepositional-phrase)))))
+  (maybe-extend (generate-word verbs)))
+
+(define (generate-prepositional-phrase)
+  (list 'prep-phrase
+	(generate-word prepositions)
+	(generate-noun-phrase)))
+
+(define nouns '(noun student professor cat class))
+(define verbs '(verb studies lectures eats sleeps))
+(define articles '(article the a))
+(define prepositions '(prep for to in by with))
 
 (define the-global-environment (setup-environment))
 (driver-loop)
