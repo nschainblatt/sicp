11d1dd62eb9427c76136bc7fce4ffe5f0a6abcac
nate <nschainblatt@gmail.com>
2025-12-28 15:20:02 -0500

    update solution

--- ch4/ex4.70/let-bindings.scm
+++ ch4/ex4.70/let-bindings.scm
@@ -26,3 +26,6 @@
 ;; Answer:
 ;; As I said before this would place the assertion twice, in the stream-car and the first element in the stream-cdr, due to
 ;; delayed evaluation having the updated value after assignment.
+
+;; Not only does this lead to repeating duplicate values, but it turns into an infinite stream. It does not keep the old
+;; ending of the stream, and instead creates a cycle, circling back to the first element repeatedly.
