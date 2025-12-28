dc68a395a5c05457dc4d967a2756a92ddd0e3adf
nate <nschainblatt@gmail.com>
2025-12-28 15:17:13 -0500

    complete exercise 4.70

---
+++ ch4/ex4.70/let-bindings.scm
@@ -0,0 +1,28 @@
+;; a.
+;; What is the purpose of the let bindings
+;; in the procedures add-assertion! and add-rule! ?
+
+;; Answer:
+;; After the set! expression, the let binding still contains the original values before the assignment.
+;; These original values are used in the delayed stream-cdr of the assignment.
+;; If we used the global variable THE-ASSERTIONS or THE-RULES instead of the let binding, we would introduce duplicate values
+;; into the stream. This is because when the stream-cdr is evaluated, the assignment would have already completed, meaning
+;; the stream-cdr when evaluated points to the updated global variable that already has the new stream-car, leading to the same
+;; value appearing twice.
+
+;; ---
+
+;; b.
+;; What would be wrong with the following implementation of add-assertion! ?
+;; Hint: Recall the definition of the infinite stream of ones in Section 3.5.2:
+;; (define ones (cons-stream 1 ones))
+
+(define (add-assertion! assertion)
+  (store-assertion-in-index assertion)
+  (set! THE-ASSERTIONS
+    (cons-stream assertion THE-ASSERTIONS))
+  'ok)
+
+;; Answer:
+;; As I said before this would place the assertion twice, in the stream-car and the first element in the stream-cdr, due to
+;; delayed evaluation having the updated value after assignment.
