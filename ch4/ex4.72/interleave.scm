03da21dad715614ffe78620ac304efec376cbaa7
nate <nschainblatt@gmail.com>
2025-12-30 21:57:24 -0500

    update solution

--- ch4/ex4.72/interleave.scm
+++ ch4/ex4.72/interleave.scm
@@ -4,7 +4,5 @@
 ;; Why did we use interleave in Section 3.5.3?)
 
 ;; The reason those procedures use interleave is to account for infinite loops in one of the streams.
-;; As we saw with ex4.71, some rules may produce infinite streams.
-;; Without interleave an infinite loop would prevent all assertions (non-rule db items) from being displayed.
-;; Since the assertions (non-infinite) and the rules (potentially infinite) are placed as the two streams respecitvely
-;; to interleave, we are guranteed to view all assertions in the database even if the rules are infinite.
+;; Without interleave an infinite loop in one of the streams would prevent all elements from the other stream from being displayed.
+;; With interleaved, we are guaranteed to see all of the elements of the non-infinite stream even if the other is infinite.
