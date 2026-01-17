1562929a8a92d0398e5d7c373806ae3985106ae5
nate <nschainblatt@gmail.com>
2026-01-17 16:25:33 -0500

    convert controllers from ex5.4 to machine for ex5.7

---
+++ ch5/ex5.7/simulate.scm
@@ -0,0 +1,54 @@
+; a. Recursive exponentiation:
+(define (expt b n)
+  (if (= n 0)
+    1
+    (* b (expt b (- n 1)))))
+
+(define expt-recursive-machine
+  (make-machine
+    '(continue n b val)
+    (list (list '= =) (list '- -) (list '* *))
+    '(assign continue (label expt-done))
+    expt-loop
+    (test (op =) (reg n) (const 0))
+    (branch (label base-case))
+    (save continue)
+    (save n)
+    (save b)
+    (assign continue (label after-expt))
+    (assign n (op -) (reg n) (const 1))
+    (goto (loop expt-loop))
+    after-expt
+    (restore b)
+    (restore n)
+    (restore continue)
+    (assign val (op *) (reg b) (reg val))
+    (goto (reg continue))
+    base-case
+    (assign val (const 1))
+    (goto (reg continue))
+    expt-done))
+
+
+; b. Iterative exponentiation:
+(define (expt b n)
+  (define (expt-iter counter product)
+    (if (= counter 0)
+      product
+      (expt-iter (- counter 1)
+		 (* b product))))
+  (expt-iter n 1))
+
+(define expt-iter-machine
+  (make-machine
+    '(n counter b product)
+    (list (list '= =) (list '- -) (list '* *))
+    (assign counter (reg n))
+    (assign product (const 1))
+    expt-loop
+    (test (op =) (reg counter) (const 0))
+    (branch (label expt-done))
+    (assign counter (op -) (reg counter) (const 1))
+    (assign product (op *) (reg b) (reg product))
+    (goto (label expt-loop))
+    expt-done))
